5ccb1ce91d2d6f2f02425f2212232f46250c4e86
Shubham Golwal <76422167+Shubham7-cell@users.noreply.github.com>
2022-10-02 21:07:27 +0530

    changed BG and added button animation

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,7 +7,7 @@ import 'animate.css';
 import TrackVisibility from "react-on-screen";
 import { isVisible } from "@testing-library/user-event/dist/utils";
 
-const name ="Crack Tech.Interviews"
+const name ="Tech."
 const info ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam luctus in tortor eu rutrum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam luctus in tortor eu rutrum."
 
 export const Banner =()=>{
@@ -15,8 +15,8 @@ export const Banner =()=>{
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const toRotate = [ "Software Developer", "Web/App Developer", "UI/UX Designer" ];
-  const period = 2000;
+  const toRotate = [ "Interviews", "Workshops", "Internships"];
+  const period = 2100;
     
     useEffect(()=>{
         let ticker=setInterval(()=>{
@@ -48,18 +48,42 @@ export const Banner =()=>{
 
     return (
       <section className="banner" id="home">
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
         <Container>
+        
           <Row className="align-items-center">
-            <Col xs={12} md={6} xl={7}>
+            <Col xs={12} md={6} xl={7 }>
               <TrackVisibility>
               {({isVisible}) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                   {/* <span className="tagline"> Welcome to my Portfolio</span> */}
-                  <h1> {name }
-                    {/* <span className="wrap">{text}</span> */}
+                  <h1 className="Crack">Crack</h1>
+                  <h1 className="Tech"> {name }
+                    <span className="wrap">{text}</span>
                   </h1>
                   <p>{info}</p>
-                  <span className="tagline"> Loreum </span>
+
+                  <span className="tagline"> 
+                  
+                  {/* <svg class="icon-arrow after" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 403.8 200.3" style="enable-background:new 0 0 403.8 200.3;" xml:space="preserve"> */}
+        <style type="text/css">
+          {/* .st0 {
+            fill: #E5EFF5;
+          } */}
+        </style>
+          <g>
+            <g>
+              <path class="st0" d="M25.2,114.3h317.3L290,166.6c-5.9,5.8-5.9,15.3-0.1,21.2c5.8,5.9,15.3,5.9,21.2,0.1l78.3-77.9c0,0,0,0,0,0
+               c5.9-5.8,5.9-15.4,0-21.2c0,0,0,0,0,0l-78.3-77.9c-5.9-5.8-15.4-5.8-21.2,0.1c-5.8,5.9-5.8,15.4,0.1,21.2l52.6,52.3H25.2
+               c-8.3,0-15,6.7-15,15C10.2,107.6,16.9,114.3,25.2,114.3z" />
+            </g>
+          </g>
+        {/* </svg> */}
+      <span>Loreum</span>
+
+                   </span>
                   <span className="tagline"> Ipsum</span>
 
                   <button onClick={() => console.log("connect")}>
